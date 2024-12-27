bcc31fb4af87109b60eeba68b412eed566903fd1
Jennifer Geis <jgeis@hawaii.edu>
2024-12-27 12:51:39 -1000

    Fixes for mangled data.  Did a manual check of every single column and it's working perfectly.

--- TEDS-D/read_tedsd_2006_2014.py
+++ TEDS-D/read_tedsd_2006_2014.py
@@ -43,6 +43,12 @@ def read_pdf(filename):
             if df is not None:
                 df['code'] = code
                 df['full_label'] = label
+                # Filter out rows with code 'PMSA'
+                df = df[df['code'] != 'PMSA'] 
+                # Filter out rows with code 'CBSA'
+                df = df[df['code'] != 'CBSA'] 
+                # Filter out rows with code 'STFIPS'
+                #df = df[df['code'] != 'STFIPS'] 
                 dfs.append(df)
             else:
                 dfs.append(pd.DataFrame.from_dict({'code': [code], 'full_label': [label]}))
@@ -105,6 +111,7 @@ def read_page(text):
     n_entries = len(entries)
     if n_entries < 5:
         return header_text.split(':'), None                     
+    #print("entries: ", entries)
 
     colways_votes = []
     if n_entries > 5:
@@ -132,15 +139,22 @@ def read_page(text):
     else:
         entries_grid = np.reshape(entries, (n_rows, 4))
 
+    # remove all records that have an empty 'Value' column entry
+    i = 0
+    while i < entries_grid.shape[0]:
+        if entries_grid[i, 0] == '' or len(entries_grid[i, 0].strip()) == 0:
+            #print("deleting: ", entries_grid[i])
+            entries_grid = np.delete(entries_grid, i, axis=0) 
+            i -= 1  # Decrement i to avoid skipping a row after deletion
+        i += 1 
+
     # Manual corrections where code = 'LOS' and bad values
     if header_text == "LOS: LENGTH OF STAY":
-        print("entries_grid: ", entries_grid)
-        
+        #print("entries_grid: ", entries_grid)
         #for i in range(entries_grid.shape[0]):
         i = 0;
         while i < entries_grid.shape[0]:
-            print('i: ', i)
-            print("entries_grid[",i,", 1]: ", entries_grid[i, 0])
+            #print("entries_grid[",i,", 1]: ", entries_grid[i, 0])
             if entries_grid[i, 0] == '81,108':
                 entries_grid[i, 0] = '25'  # update Value
                 entries_grid[i, 1] = '25'  # Update Label
@@ -188,6 +202,174 @@ def read_page(text):
                 i -= 1  # Decrement i to avoid skipping a row after deletion
             i += 1 
 
+    # Manual corrections where code = 'DAYWAIT' and bad values
+    if header_text == "DAYWAIT: DAYS WAITING TO ENTER TREATMENT":
+        #print("entries_grid: ", entries_grid)
+        #for i in range(entries_grid.shape[0]):
+        i = 0;
+        while i < entries_grid.shape[0]:
+            #print("entries_grid[",i,", 0]: ", entries_grid[i, 0], ", entries_grid[",i,", 1]: ", entries_grid[i, 1])
+            if entries_grid[i, 0] == '34' and entries_grid[i, 1] == '36':
+                entries_grid[i, 1] = '34'
+            elif entries_grid[i, 0] == '35' and entries_grid[i, 1] == '37':
+                entries_grid[i, 1] = '35'  
+            elif entries_grid[i, 0] == '36':
+                entries_grid[i, 1] = '36'  
+            elif entries_grid[i, 0] == '37':
+                entries_grid[i, 1] = '37' 
+            elif entries_grid[i, 0] == '38':
+                entries_grid[i, 1] = '38' 
+            elif entries_grid[i, 0] == '39':
+                entries_grid[i, 1] = '39' 
+            elif entries_grid[i, 0] == '40':
+                entries_grid[i, 1] = '40' 
+            elif entries_grid[i, 0] == '41':
+                entries_grid[i, 1] = '41'   
+            elif entries_grid[i, 0] == '42':
+                entries_grid[i, 1] = '42' 
+            elif entries_grid[i, 0] == '43':
+                entries_grid[i, 1] = '43' 
+            elif entries_grid[i, 0] == '44':
+                entries_grid[i, 1] = '44' 
+            elif entries_grid[i, 0] == '45':
+                entries_grid[i, 1] = '45' 
+            elif entries_grid[i, 0] == '46':
+                entries_grid[i, 1] = '46' 
+            elif entries_grid[i, 0] == '47':
+                entries_grid[i, 1] = '47' 
+            elif entries_grid[i, 0] == '48':
+                entries_grid[i, 1] = '48' 
+            elif entries_grid[i, 0] == '49':
+                entries_grid[i, 1] = '49' 
+            elif entries_grid[i, 0] == '34' and entries_grid[i, 1] == '10,088':
+                entries_grid = np.delete(entries_grid, i, axis=0) 
+                i -= 1 # Decrement i to avoid skipping a row after deletion
+            elif entries_grid[i, 0] == '35' and entries_grid[i, 1] == '13,607':
+                entries_grid = np.delete(entries_grid, i, axis=0) 
+                i -= 1  # Decrement i to avoid skipping a row after deletion
+
+                # adding missing rows here instead of outside the loop because DAYWAIT runs over two separate pages
+                # so it would get executed twice.  This way, it appears at the end of all the DAYWAITS
+                new_row = ['X', 'OTHER DAYWAIT CATEGORIES NOT PRINTED HERE', '', '']
+                entries_grid = np.append(entries_grid, [new_row], axis=0)
+                new_row = ['-9', 'MISSING/UNKNOWN/NOT COLLECTED/INVALID', '', '']
+                entries_grid = np.append(entries_grid, [new_row], axis=0)
+                i += 2
+            i += 1 
+
+    if header_text == "REASON: REASON FOR DISCHARGE":
+        i = 0
+        while i < entries_grid.shape[0]:
+            #print("entries_grid[",i,", 0]: ", entries_grid[i, 0], ", entries_grid[",i,", 1]: ", entries_grid[i, 1])
+            if entries_grid[i, 0] == '1':
+                entries_grid[i, 1] = 'Treatment Completed'
+            elif entries_grid[i, 0] == '2':
+                entries_grid[i, 1] = 'Left Against Professional Advice'
+            elif entries_grid[i, 0] == '3':
+                entries_grid[i, 1] = 'Terminated By Facility'
+            elif entries_grid[i, 0] == '4':
+                entries_grid[i, 1] = 'Transferred To Another Treatment Program Or Facility'
+            elif entries_grid[i, 0] == '5':
+                entries_grid[i, 1] = 'Incarcerated'
+            elif entries_grid[i, 0] == '6':
+                entries_grid[i, 1] = 'Death'
+            elif entries_grid[i, 0] == '7':
+                entries_grid[i, 1] = 'Other'
+            elif entries_grid[i, 0] == 'TREATMENT COMPLETED':
+                entries_grid[i, 0] = '-9'
+                entries_grid[i, 1] = 'Missing/Unknown/Not Collected/Invalid'
+            i += 1 
+
+    if header_text == "PRIMPAY: EXPECTED/ACTUAL PRIMARY SOURCE OF PAYMENT":
+        i = 0
+        while i < entries_grid.shape[0]:
+            #print("entries_grid[",i,", 0]: ", entries_grid[i, 0], ", entries_grid[",i,", 1]: ", entries_grid[i, 1])
+            if entries_grid[i, 0] == '1':
+                entries_grid[i, 1] = 'Self-Pay'
+            elif entries_grid[i, 0] == '2':
+                entries_grid[i, 1] = 'Blue Cross/Blue Shield, Other Health Insurance Companies'
+            elif entries_grid[i, 0] == '3':
+                entries_grid[i, 1] = 'Medicare, Workers Compensation'
+            elif entries_grid[i, 0] == '4':
+                entries_grid[i, 1] = 'Medicaid'
+            elif entries_grid[i, 0] == '5':
+                entries_grid[i, 1] = 'Other Government Payments'
+            elif entries_grid[i, 0] == '8':
+                entries_grid[i, 1] = 'No Charge (Free, Charity, Special Research, Teaching)'
+            elif entries_grid[i, 0] == '9':
+                entries_grid[i, 1] = 'Other'
+            elif entries_grid[i, 0] == 'SELF-PAY':
+                entries_grid[i, 0] = '-9'
+                entries_grid[i, 1] = 'Missing/Unknown/Not Collected/Invalid'
+            i += 1 
+    
+    if header_text == "SERVSETD: SERVICE SETTING AT DISCHARGE":
+        i = 0
+        while i < entries_grid.shape[0]:
+            #print("entries_grid[",i,", 0]: ", entries_grid[i, 0], ", entries_grid[",i,", 1]: ", entries_grid[i, 1])
+            if entries_grid[i, 0] == '1':
+                entries_grid[i, 1] = 'DETOX, 24 HR, HOSPITAL INPATIENT'
+            elif entries_grid[i, 0] == '2':
+                entries_grid[i, 1] = 'DETOX, 24 HR, FREE-STANDING RESIDENTIAL'
+            elif entries_grid[i, 0] == '3':
+                entries_grid[i, 1] = 'REHAB/RES, HOSPITAL (NON-DETOX)'
+            elif entries_grid[i, 0] == '4':
+                entries_grid[i, 1] = 'REHAB/RES, SHORT TERM (30 DAYS OR FEWER)'
+            elif entries_grid[i, 0] == '5':
+                entries_grid[i, 1] = 'REHAB/RES, LONG TERM (MORE THAN 30 DAYS)'
+            elif entries_grid[i, 0] == '6':
+                entries_grid[i, 1] = 'AMBULATORY, INTENSIVE OUTPATIENT'
+            elif entries_grid[i, 0] == '7':
+                entries_grid[i, 1] = 'AMBULATORY, NON-INTENSIVE OUTPATIENT'
+            elif entries_grid[i, 0] == '8':
+                entries_grid[i, 1] = 'AMBULATORY, DETOXIFICATION'   
+            elif entries_grid[i, 0] == 'DETOX, 24 HR, HOSPITAL INPATIENT':
+                entries_grid[i, 0] = '-9'
+                entries_grid[i, 1] = 'Missing/Unknown/Not Collected/Invalid'
+            i += 1 
+
+    # format years from something like '12-14' to match other years like '12 To 14 Years' 
+    if header_text.startswith('FRSTUSE'):
+        #print("entries_grid: ", entries_grid)
+        #for i in range(entries_grid.shape[0]):
+        i = 0
+        while i < entries_grid.shape[0]:
+            #print("entries_grid[",i,", 1]: ", entries_grid[i, 0])
+            match = re.match(r"(\d+)-(\d+)", entries_grid[i, 1])
+            if match:
+                start, end = match.groups()
+                entries_grid[i, 1] = f"{start} to {end} years"
+            i += 1
+
+    if header_text == "PSOURCE: PRINCIPAL SOURCE OF REFERRAL":
+        #print("entries_grid: ", entries_grid)
+        #for i in range(entries_grid.shape[0]):
+        i = 0
+        while i < entries_grid.shape[0]:
+            #print("entries_grid[",i,", 1]: ", entries_grid[i, 0])
+            if entries_grid[i, 0] == '1':
+                entries_grid[i, 1] = 'INDIVIDUAL (INCLUDES SELF-REFERRAL)'
+            elif entries_grid[i, 0] == '2':
+                entries_grid[i, 1] = 'ALCOHOL/DRUG ABUSE CARE PROVIDER'
+            elif entries_grid[i, 0] == '3':
+                entries_grid[i, 1] = 'OTHER HEALTH CARE PROVIDER'
+            elif entries_grid[i, 0] == '4':
+                entries_grid[i, 1] = 'SCHOOL (EDUCATIONAL)'
+            elif entries_grid[i, 0] == '5':
+                entries_grid[i, 1] = 'EMPLOYER/EAP'
+            elif entries_grid[i, 0] == '6':
+                entries_grid[i, 1] = 'OTHER COMMUNITY REFERRAL'
+            elif entries_grid[i, 0] == '7':
+                entries_grid[i, 1] = 'COURT/CRIMINAL JUSTICE REFERRAL / DUI / DWI'  # Update Label
+            elif entries_grid[i, 0] == 'INDIVIDUAL (INCLUDES SELF-REFERRAL)':
+                entries_grid[i, 0] = '-9'
+            i += 1 
+        #new_row = ['-9', 'MISSING/UNKNOWN/NOT COLLECTED/INVALID', '', '']
+        #print("executed")
+        #entries_grid = np.append(entries_grid, [new_row], axis=0)
+
+
+    #print("entries: ", entries_grid)
     df = pd.DataFrame(entries_grid,
                   columns=['Value', 'Label', 'Frequency', 'Percent'])
     
