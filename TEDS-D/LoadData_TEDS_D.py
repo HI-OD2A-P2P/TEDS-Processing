6bdcce7782ba418cff4a8d733355c24dc55e0dd8
Jennifer Geis <jgeis@hawaii.edu>
2025-04-09 15:02:17 -1000

    1.) Fixed bug where ints were getting turned into floats. 2.) Added saving of any existing output file to a storage directory.

--- TEDS-D/LoadData_TEDS_D.py
+++ TEDS-D/LoadData_TEDS_D.py
@@ -29,6 +29,8 @@ from mysql.connector import Error
 import sqlalchemy as sa
 from sqlalchemy import create_engine, inspect
 import json
+import shutil
+from datetime import datetime
 
 current_file_directory = os.path.dirname(os.path.abspath(__file__))
 
@@ -67,17 +69,26 @@ if test_mode:
     print(f"db_user: {db_user}")
     print(f"db_pwd: {db_pwd}")
 
+def backup_existing_combined_file():
+    # Define the file path for the combined data file
+    combined_file_path = os.path.join(dir, "combined_data.csv")
+
+    # Check if the file exists
+    if os.path.exists(combined_file_path):
+        # Generate a timestamp for the backup file name
+        timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+        backup_file_path = os.path.join(dir, "storage/", f"combined_data_{timestamp}.csv")
+
+        # Move the existing file to the backup file
+        shutil.move(combined_file_path, backup_file_path)
+        print(f"Existing combined_data.csv moved to {backup_file_path}")
+    else:
+        print("No existing combined_data.csv file found.")
+
+
 # merges all .csv files found in the <dir> into one csv named <fileName>
 def combine_csv_files():
     print("Running combine_csv_files")
-
-    # Check if the file exists
-    if os.path.exists(full_file_path):
-        print(f"The file {full_file_path} already exists. Skipping the combination process.")
-        return
-        # If the file exists, delete it
-        #os.remove(full_file_path)
-        #print("Pre-existing output file deleted")
 
     # Get a list of all files in the directory
     all_files = os.listdir(dir)
@@ -96,16 +107,17 @@ def combine_csv_files():
         # TODO: need to normalize the columns as they differ across the years. For all the columns for each year, see:
         # https://docs.google.com/spreadsheets/d/1Jrto9Tl0w27WwdSPLtSFg6VpsZzC7BpWjwkNaKYtYcY/edit#gid=0
 
-        # from non-2021 file
-        #col_types = {"DISYR":"string","CASEID":"string","STFIPS":"string","CBSA2010":"string","EDUC":"string","MARSTAT":"string","SERVICES":"string","DETCRIM":"string","LOS":"string","PSOURCE":"string","NOPRIOR":"string","ARRESTS":"string","EMPLOY":"string","METHUSE":"string","PSYPROB":"string","PREG":"string","GENDER":"string","VET":"string","LIVARAG":"string","DAYWAIT":"string","SERVICES_D":"string","REASON":"string","EMPLOY_D":"string","LIVARAG_D":"string","ARRESTS_D":"string","DSMCRIT":"string","AGE":"string","RACE":"string","ETHNIC":"string","DETNLF":"string","DETNLF_D":"string","PRIMINC":"string","SUB1":"string","SUB2":"string","SUB3":"string","SUB1_D":"string","SUB2_D":"string","SUB3_D":"string","ROUTE1":"string","ROUTE2":"string","ROUTE3":"string","FREQ1":"string","FREQ2":"string","FREQ3":"string","FREQ1_D":"string","FREQ2_D":"string","FREQ3_D":"string","FRSTUSE1":"string","FRSTUSE2":"string","FRSTUSE3":"string","HLTHINS":"string","PRIMPAY":"string","FREQ_ATND_SELF_HELP":"string","FREQ_ATND_SELF_HELP_D":"string","ALCFLG":"string","COKEFLG":"string","MARFLG":"string","HERFLG":"string","METHFLG":"string","OPSYNFLG":"string","PCPFLG":"string","HALLFLG":"string","MTHAMFLG":"string","AMPHFLG":"string","STIMFLG":"string","BENZFLG":"string","TRNQFLG":"string","BARBFLG":"string","SEDHPFLG":"string","INHFLG":"string","OTCFLG":"string","OTHERFLG":"string","DIVISION":"string","REGION":"string","IDU":"string","ALCDRUG":"string","CBSA":"string","PMSA":"string","SERVSETD":"string","NUMSUBS":"string","YEAR":"string"}
-        # 2021 file  
-        #col_types = {"YEAR":"double","NUMSUBS":"double","SERVSETD":"double","PMSA":"double","CBSA":"double","CBSA2010":"double","DISYR": "int","CASEID": "int","STFIPS": "string","EDUC": "string","MARSTAT": "string","SERVICES": "string","DETCRIM": "string","LOS": "string","PSOURCE": "string","NOPRIOR": "string","ARRESTS": "string","EMPLOY": "string","METHUSE": "string","PSYPROB": "string","PREG": "string","GENDER": "string","VET": "string","LIVARAG": "string","DAYWAIT": "string","SERVICES_D": "string","REASON": "string","EMPLOY_D": "string","LIVARAG_D": "string","ARRESTS_D": "string","DSMCRIT": "string","AGE": "string","RACE": "string","ETHNIC": "string","DETNLF": "string","DETNLF_D": "string","PRIMINC": "string","SUB1": "string","SUB2": "string","SUB3": "string","SUB1_D": "string","SUB2_D": "string","SUB3_D": "string","ROUTE1": "string","ROUTE2": "string","ROUTE3": "string","FREQ1": "string","FREQ2": "string","FREQ3": "string","FREQ1_D": "string","FREQ2_D": "string","FREQ3_D": "string","FRSTUSE1": "string","FRSTUSE2": "string","FRSTUSE3": "string","HLTHINS": "string","PRIMPAY": "string","FREQ_ATND_SELF_HELP": "string","FREQ_ATND_SELF_HELP_D": "string","ALCFLG": "string","COKEFLG": "string","MARFLG": "string","HERFLG": "string","METHFLG": "string","OPSYNFLG": "string","PCPFLG": "string","HALLFLG": "string","MTHAMFLG": "string","AMPHFLG": "string","STIMFLG": "string","BENZFLG": "string","TRNQFLG": "string","BARBFLG": "string","SEDHPFLG": "string","INHFLG": "string","OTCFLG": "string","OTHERFLG": "string","DIVISION": "string","REGION": "string","IDU": "string","ALCDRUG": "string","CBSA2020": "string"}
+        col_types = {"YEAR":"int","DISYR":"int","CASEID":"int","STFIPS":"int","CBSA":"int","CBSA2010":"int","CBSA2020":"int","EDUC":"int","MARSTAT":"int","SERVSETD":"int","SERVICES":"int","DETCRIM":"int","LOS":"int","PSOURCE":"int","NOPRIOR":"int","ARRESTS":"int","EMPLOY":"int","METHUSE":"int","PSYPROB":"int","PREG":"int","GENDER":"int","VET":"int","LIVARAG":"int","DAYWAIT":"int","SERVICES_D":"int","REASON":"int","EMPLOY_D":"int","LIVARAG_D":"int","ARRESTS_D":"int","DSMCRIT":"int","AGE":"int","RACE":"int","ETHNIC":"int","DETNLF":"int","DETNLF_D":"int","PRIMINC":"int","SUB1":"int","SUB2":"int","SUB3":"int","SUB1_D":"int","SUB2_D":"int","SUB3_D":"int","ROUTE1":"int","ROUTE2":"int","ROUTE3":"int","FREQ1":"int","FREQ2":"int","FREQ3":"int","FREQ1_D":"int","FREQ2_D":"int","FREQ3_D":"int","FRSTUSE1":"int","FRSTUSE2":"int","FRSTUSE3":"int","HLTHINS":"int","PRIMPAY":"int","FREQ_ATND_SELF_HELP":"int","FREQ_ATND_SELF_HELP_D":"int","ALCFLG":"int","COKEFLG":"int","MARFLG":"int","HERFLG":"int","METHFLG":"int","OPSYNFLG":"int","PCPFLG":"int","HALLFLG":"int","MTHAMFLG":"int","AMPHFLG":"int","STIMFLG":"int","BENZFLG":"int","TRNQFLG":"int","BARBFLG":"int","SEDHPFLG":"int","INHFLG":"int","OTCFLG":"int","OTHERFLG":"int","DIVISION":"int","REGION":"int","IDU":"int","ALCDRUG":"int","PMSA":"int","NUMSUBS":"int"}
+        # these columns are not found in certain years (looking at you, 2007).  So need to add them in, 
+        # otherwise we get a weird artifact where it makes the existing values in those columns into floats instead of ints 
+        required_columns = ['SERVICES', 'ARRESTS', 'SERVICES_D', 'EMPLOY_D', 'LIVARAG_D', 'ARRESTS_D', 'DETNLF_D', 'SUB1_D', 'SUB2_D', 'SUB3_D', 'FREQ1_D', 'FREQ2_D', 'FREQ3_D', 'FREQ_ATND_SELF_HELP', 'FREQ_ATND_SELF_HELP_D']
 
         data = pd.DataFrame()
         # the formats between years change, so some have text some are numeric, handle both.
         if ("2021" in file_path):
             print("2021")
             # reading tedsd_puf_2021.csv fails unless encoding='latin-1'
+            # don't provide the coltypes since this stupidly inconsistent year is already decoded and it's all strings
             data = pd.read_csv(file_path, low_memory=False, encoding='latin-1')
 
             # strip out everything but Hawaii as the end result was just too big
@@ -113,14 +125,16 @@ def combine_csv_files():
 
             # normalize column names
             data_hawaii = data_hawaii.rename(columns={"CBSA2020": "CBSA"})
-            print("inserting rows: ", len(data_hawaii))
-
+            print("inserting rows: ", len(data_hawaii))    
             combined_data = pd.concat([combined_data, data_hawaii], axis="rows")
+            #if 'ARRESTS' in combined_data.columns:
+            #    print("2. Unique values in ARRESTS:", combined_data['ARRESTS'].unique())    
+
         elif ("2015" in file_path
               or "2016" in file_path):
             print("2015 or 2016")
             # this works for non-2021, but not 2021
-            data = pd.read_csv(file_path, encoding='latin-1')
+            data = pd.read_csv(file_path, dtype=col_types, encoding='latin-1')
 
             # strip out everything but Hawaii as the end result was just too big
             data_hawaii = data[data['STFIPS'] == 15]
@@ -134,15 +148,13 @@ def combine_csv_files():
             #print("combined_data: ", len(combined_data), "data_hawaii: ", len(data_hawaii))
             combined_data = pd.concat([combined_data, data_hawaii], axis="rows").fillna("")
             #print("combined_data: ", len(combined_data), "data_hawaii: ", len(data_hawaii))
-
         elif ("2017" in file_path
               or "2018" in file_path
               or "2019" in file_path
               or "2020" in file_path): 
-            print("2017-2020")
+            print("2017-2020: ", file_path)
             # this works for non-2021, but not 2021
-            data = pd.read_csv(file_path, encoding='latin-1')
-
+            data = pd.read_csv(file_path, dtype=col_types, encoding='latin-1')
             # strip out everything but Hawaii as the end result was just too big
             data_hawaii = data[data['STFIPS'] == 15] 
             # normalize column names
@@ -151,7 +163,7 @@ def combine_csv_files():
             combined_data = pd.concat([combined_data, data_hawaii], axis="rows").fillna("")         
         elif ("2022" in file_path): 
             print("2022")
-            data = pd.read_csv(file_path, encoding='latin-1')
+            data = pd.read_csv(file_path, dtype=col_types, encoding='latin-1')
 
             # strip out everything but Hawaii as the end result was just too big
             data_hawaii = data[data['STFIPS'] == 15] 
@@ -170,22 +182,29 @@ def combine_csv_files():
               or "2014" in file_path): 
             print("2006-2014")
             # this works for non-2021, but not 2021
-            data = pd.read_csv(file_path, encoding='latin-1')
+            data = pd.read_csv(file_path, dtype=col_types, encoding='latin-1')
 
             # strip out everything but Hawaii as the end result was just too big
             data_hawaii = data[data['STFIPS'] == 15]
             # normalize column names    
             data_hawaii = data_hawaii.rename(columns={"SERVSETD": "SERVICES"})
+
+            # Ensure all required columns exist
+            for col in required_columns:
+                if col not in data_hawaii.columns:
+                    data_hawaii[col] = -9  # Add the missing column with a default value
+
             #data_hawaii = data_hawaii.drop(columns=['NUMSUBS'])
             #data_hawaii = data_hawaii.drop(columns=['PMSA'])
             print("inserting rows: ", len(data_hawaii))
-
             combined_data = pd.concat([combined_data, data_hawaii], axis="rows").fillna("")
 
     # TODO: some of the data has a space before the value, need to strip off whitespace
 
     # Write the combined data to a new .csv file
     if not test_mode:
+        # Call the function before creating a new combined_data.csv
+        backup_existing_combined_file()
         print("inserting total rows: ", len(combined_data))
         combined_data.to_csv(full_file_path, index=False)
     else:
